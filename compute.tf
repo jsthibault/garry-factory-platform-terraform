dd4bd00713f7098c5b8b030d43f8052478de7dee
Jean-Stephane <jean-stephane.thibault@adevinta.com>
2026-01-07 17:46:04 +0100

    feat(firewall): add firewall

--- compute.tf
+++ compute.tf
@@ -1,8 +1,43 @@
+resource "hcloud_firewall" "garry_factory" {
+  name = "garry-factory-firewall"
+
+  labels = {
+    project    = "garry-factory"
+    managed_by = "terraform"
+  }
+
+  rule {
+    description = "SSH"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "22"
+    source_ips  = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    description = "App port 8080"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "8080"
+    source_ips  = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    description = "App port 8081"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "8081"
+    source_ips  = ["0.0.0.0/0", "::/0"]
+  }
+}
+
 resource "hcloud_server" "garry_factory_1" {
   name        = "garry-factory-1"
   image       = var.server_image
   server_type = var.server_type
   location    = var.server_location
+
+  firewall_ids = [hcloud_firewall.garry_factory.id]
 
   labels = {
     project     = "garry-factory"
